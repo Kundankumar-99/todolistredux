9279d719d8d15d91be5cb44d3f8cec3858ca2bef
kundan kumar <kundankumarkondil1999@gmail.com>
2024-10-17 12:12:05 +0530

    changes update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import AddTodo from "./components/Addtodo";
+import Todos from "./components/Todos";
 
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <AddTodo/>
+      <Todos/>
+      
     </>
   );
 }
--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import todoReducers from '../features/Todo/todoslice'
 
 export const store = configureStore({
+
+reducer : todoReducers
 
 })
 
